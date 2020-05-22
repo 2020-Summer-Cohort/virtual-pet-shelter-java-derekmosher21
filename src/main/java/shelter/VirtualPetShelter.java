e607c4edd9b14a2939d5ac9f27f3128c7d4893a0
Derek Mosher <derekmosher21@gmail.com>
2020-05-22 16:57:41 -0400

    half way with VirtualPetShelter class

--- src/main/java/shelter/VirtualPetShelter.java
+++ src/main/java/shelter/VirtualPetShelter.java
@@ -1,6 +1,34 @@
 package shelter;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 public class VirtualPetShelter {
+    private Map<String, VirtualPet> pets = new HashMap<>();
+
+    public Collection<VirtualPet> getAllPets() { return pets.values();}
+
+    public VirtualPet getVirtualPet(String petName) { return pets.get(petName); }
+
+    public void addPet (VirtualPet petToAdd ) { pets.put(petToAdd.getPetName(), petToAdd); }
+
+    public void removePet (String petName) { pets.remove(petName); }
+
+    public void feedPets (int hunger) {
+        return VirtualPet.getHunger
+    }
 
 
 }
+
+// [ ] include appropriate instance variable(s) to store the pets who reside at the shelter
+//- [ ] include methods that:
+//	- [ ] return a `Collection` of all of the pets in the shelter
+//	- [ ] return a specific `VirtualPet` given its name
+//	- [ ] allow intake of a homeless pet (adding a pet to the shelter)
+//	- [ ] allow adoption of a homeless pet (removing a pet from the shelter)
+//	- [ ] feed all of the pets in the shelter
+//	- [ ] water all of the pets in the shelter
+//	- [ ] plays (or performs some other interaction(s)) with an individual pet in the shelter
+//- [ ] include a `tick` method that calls the `tick` method for each of the pets in the shelter, updating their needs
--- src/main/java/shelter/VirtualPetShelterApp.java
+++ src/main/java/shelter/VirtualPetShelterApp.java
@@ -3,6 +3,8 @@ package shelter;
 public class VirtualPetShelterApp {
     public static void main(String[] args) {
 
+    VirtualPet martin = new VirtualPet(8, 6, 12, "Martin", "Young and feisty.");
+    VirtualPet oliver = new VirtualPet(9, 4, 4, "Oliver", "Cute, Cuddly, and loud.");
 
     }
 }
